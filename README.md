1a62f0239fd33b4a2022355ba8f6c24f03ecec05
MoFap <morenikefapojuwo77@gmail.com>
2021-05-31 10:48:53 -0500

    django changes

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # CurrentDjango_Project
 This is the Django Models Project
+
+![Image of Admin Panel](DJangoadminpanel.png)
--- myproject/theapp/models.py
+++ myproject/theapp/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.db.models.deletion import CASCADE
-from datetime import datetime
+from django.utils import timezone
+
 
 # Create your models here.
 
@@ -15,20 +16,36 @@ class School(models.Model):
 class Faculty(models.Model):
     name = models.CharField(max_length=300)
 
+    def __str__(self):
+        return self.name
+
 class Department(models.Model):
     name = models.CharField(max_length=400)
     faculty = models.ForeignKey(Faculty, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.name
+
 class Certificate_type(models.Model):
     name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
 
 class Grade(models.Model):
     type = models.CharField(max_length=4)
 
+    def __str__(self):
+        return self.type
+
 class Student(models.Model):
     fullname = models.CharField(max_length=40)
-    year_of_graduation = models.DateTimeField(default=datetime.today)
+    year_of_graduation = models.DateTimeField(default=timezone.now)
     department = models.ForeignKey(Department, on_delete=models.CASCADE)
     grade = models.ForeignKey(Grade, on_delete=models.CASCADE)
     school = models.ForeignKey(School, on_delete=models.CASCADE)
     certificate_type = models.ForeignKey(Certificate_type, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.fullname
